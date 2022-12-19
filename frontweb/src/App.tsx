34c4890540d6350ecba81c805e914f8c22568643
Luciana S Bianchini <lsouzabianchini@gmail.com>
2022-12-19 16:39:40 -0300

    Navbar Styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,14 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
-function App() {
+const App = () => {
   return (
-<h1>Hello Movie Flix</h1>
+    <>
+  <Navbar/>
+  <h1>Hello Movie Flix</h1>
+  </>
   );
-}
+};
 
 export default App;
